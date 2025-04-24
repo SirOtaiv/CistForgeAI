bab74122976e6dd6481e02caa931e2814271f27f
SirOtaiv <rauotavio7@gmail.com>
2025-04-23 21:30:46 -0300

    Primeiro teste de implementação da Lib OpenCV

--- Services/CistForge/test.py
+++ Services/CistForge/test.py
@@ -1,5 +1,5 @@
 import cv2 as cv
-img = cv.imread("C:\Users\Otavio\Pictures\Noob Corp\NoobsCorpScreenHoot.jpg")
+img = cv.imread("../../Temp/images/NoobsCorpScreenHoot.jpg")
 
 cv.imshow("Display window", img)
 k = cv.waitKey(0) # Wait for a keystroke in the window
