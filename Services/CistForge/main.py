9a212ba7924d6bf7e3822a5583b9554fcf29fa49
SirOtaiv <rauotavio7@gmail.com>
2025-05-12 22:24:27 -0300

    Configurado pontos para geração dos desenhos

--- Services/CistForge/main.py
+++ Services/CistForge/main.py
@@ -1,18 +1,49 @@
 import cv2 as cv
 import numpy as np
 
-imgHeigh, imgWidth = 400, 400
-thicknes, color, topPoint = 3, (0, 0, 0), 60 # Espessura 6, Cor Black
+x = 400
+thicknes, color, zPoint = 3, (0, 0, 0), 60
+
+aPoint, bPoint = (x // 2), (x - zPoint)
+tPoint = bPoint // 4
+
+pointBlocks = [
+    (aPoint - tPoint, zPoint),             # 1
+    (aPoint, zPoint),                      # 2
+    (aPoint + tPoint, zPoint),             # 3
+
+    (aPoint - tPoint, zPoint + tPoint),    # 4
+    (aPoint, zPoint + tPoint),             # 5
+    (aPoint + tPoint, zPoint + tPoint),    # 6
+
+    (aPoint - tPoint, bPoint - tPoint),    # 7
+    (aPoint, bPoint - tPoint),             # 8
+    (aPoint + tPoint, bPoint - tPoint),    # 9
+
+    (aPoint - tPoint, bPoint),             # 10
+    (aPoint, bPoint),                      # 11
+    (aPoint + tPoint, bPoint),             # 12
+    ]
 
 # Pontos de Referencia para geracao do pino central da imagem
-startPoint, endPoint = (imgWidth//2, topPoint), (imgWidth//2, imgHeigh - 2*topPoint)
+startPoint, endPoint = pointBlocks[1], pointBlocks[10]
 
-firstImage = np.ones((imgHeigh, imgWidth, 3), dtype=np.uint8) * 255
+firstImage = np.ones((x, x, 3), dtype=np.uint8) * 255
 
-cv.line(firstImage, startPoint, endPoint, color, thicknes)
+# Criando a linha central
+cv.line(firstImage, pointBlocks[1], pointBlocks[10], color, thicknes)
 
-# Separação dos 4 quadrantes
-cv.line(firstImage, startPoint, ((imgWidth//2) - (imgHeigh - 2*topPoint)//4, (imgHeigh - 2*topPoint)//4 + 30), (0, 0, 255), thicknes)
+# Printando numero 1
+cv.line(firstImage, pointBlocks[1], pointBlocks[2], color, thicknes)
+
+# Printando numero 20
+cv.line(firstImage, pointBlocks[3], pointBlocks[4], color, thicknes)
+
+# Printando numero 400
+cv.line(firstImage, pointBlocks[7], pointBlocks[11], color, thicknes)
+
+# Printando numero 6000
+cv.line(firstImage, pointBlocks[6], pointBlocks[9], color, thicknes)
 
 cv.imshow("Hello World", firstImage)
 k = cv.waitKey(0)
